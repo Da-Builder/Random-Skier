6a0ef3afe8302f8691debc3d084569995c65d58e
Alex Xu <Da.Alexxu@gmail.com>
2026-04-14 02:51:42 +0000

    Update: Terraform to use variables

--- iac/main.tf
+++ iac/main.tf
@@ -1,4 +1,22 @@
 provider "aws" {
-  region = "us-east-1" # Northern Virginia USA
-  default_tags { tags = { Project = "Ski" } }
+  region = var.region
+  default_tags { tags = { Project = title(var.project) } }
+}
+
+
+variable "region" {
+  type        = string
+  description = "The AWS region to deploy infrastructure"
+  default     = "ap-southeast-2" # Sydney Australia
+}
+
+variable "project" {
+  type        = string
+  description = "The project name used for tags & prefixes"
+  default     = "ski"
+
+  validation {
+    condition     = var.project == lower(var.project)
+    error_message = "Invalid Project Name: Must be lowercase"
+  }
 }
